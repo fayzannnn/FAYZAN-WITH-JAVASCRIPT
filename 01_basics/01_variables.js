3d1f66d2bac608acff743021d04ae1242ab9806d
fayzannnn <babushikalgar70@gmail.com>
2026-09-20 17:11:35 +0530

    js initiated

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountID = 223344
+
+let accountEmail = "fzn@gmail.com"
+
+var accountPassword = "12345"
+
+accountCity = "mumbai"
+accountState = undefined 
+
+// console.log(accountEmail);
+
+accountEmail = "abc@gmail.com"
+console.log(accountEmail);
+
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"
+
+
+// alert(3+2)         // error bcause we are using node not browser
+
+// console.log(3+2) , console.log("fayzan");
+
+//  console.log(3+2);
+//  console.log("fayzan");               // better code readability 
+ 
+ 
+ // DATATYPES  
+
+
+ let name = "fayzan"
+ let age = 22
+ let isloogedIn = false
+ let state = undefined
+
+
+ console.log(typeof name);
+  console.log(typeof age);
+   console.log(typeof isloogedIn);
+    console.log(typeof state);
+
+
+  console.log(typeof null);
