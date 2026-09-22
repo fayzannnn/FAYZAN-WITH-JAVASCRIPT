2e975a60cc8e1c8c93217ee1018678e2c9a35a27
fayzannnn <babushikalgar70@gmail.com>
2026-09-22 15:20:17 +0530

    js basics

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,97 @@
+//  let score = 33
+//  console.log(typeof score);
+ 
+
+ let scoore = "33"
+//  console.log(typeof scoore);
+ 
+
+ let valueInNumber = Number(scoore)
+//  console.log(typeof valueInNumber);
+ 
+
+ let sccore = "33abc"
+ let valueINnumber = Number(sccore)
+//  console.log(valueINnumber);
+ 
+//  console.log(typeof valueINnumber);
+ 
+
+let scorre = null 
+// console.log(typeof scorre)
+// console.log(scorre);
+
+
+// let score = true
+// let valueeInNumber = Number(score)
+// console.log(typeof valueeInNumber);
+// console.log(valueeInNumber);
+
+// let score = false
+// let valueeInNumber = Number(score)
+// console.log(typeof valueeInNumber);
+// console.log(valueeInNumber);
+
+// let score = "fayzan"
+// console.log( typeof score);
+// let valueeInNumber = Number(score)
+// console.log(typeof valueeInNumber);
+// console.log(valueeInNumber);
+
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(typeof someNumber);
+// console.log(someNumber);
+// console.log(typeof stringNumber);
+
+
+
+//  OPERATIONS
+
+
+// let value = 3
+// let negativeValue = -value 
+// console.log(negativeValue);
+
+
+
+// console.log(2+2);    //addtion
+// console.log(3-2);    // subtraction
+// console.log(2*2);    // multiplication
+// console.log(2**3);    // power 
+// console.log(2/3);     // division
+// console.log(2%3);     // check remainder 
+
+
+
+let str1 = 'hello '
+let str2 = 'fayzan'
+let str3= (str1+str2)
+console.log(str3);
+
+//complex situation 
+
+// console.log("1"+ 2 );
+// console.log( 1 +'2');
+// console.log("1"+2 + 2);
+// console.log( 1 + 2 +'2');
+
+
+// tricky conversions 
+
+// console.log(+ true);             // => 1
+// console.log(+ " ");              //  => 0 
+
+
+let num1,num2,num3
+num1=num2=num3=2+2
+console.log(num2);
+
+
+
+let gameCounter = 100 
+ ++gameCounter  // gameCounter++
+console.log(gameCounter);
+
+
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,31 @@
+// // console.log(2>1);
+// // console.log(2<1);
+// // console.log(2>=1);
+// // console.log(2<=1);
+// // console.log(2==1);
+// // console.log(2!=1);
+
+
+// console.log("2">1);      // js convert string into number so make sure to use same type of data  
+// console.log("02">1);
+
+
+// console.log(null>0);          // equalitycheck and comparision check both works diffrently 
+// console.log(null==0);                                                           //  null => number
+// console.log(null>=0);
+
+
+// console.log(undefined >0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+
+
+// note :  comparision and equality are two different things in javascript works on diggerent syntax 
+
+
+
+// STRICK CHECK 
+
+console.log("2"=== 2);      // => false  its checks both datatype and value 
+ 
+console.log(2===2);
